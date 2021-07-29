0d8589dcc68bf27ae199f9109f71b8ecc24ea437
builder35abhishek <abhishek.gharat04@gmail.com>
2021-07-30 01:57:21 +0530

    Update install_app.sh

--- install_app.sh
+++ install_app.sh
@@ -10,10 +10,8 @@ db_username="$2"
 db_password="$3"
 db_name="$4"
 
-mysql -h $db_ip -u $db_username -p${db_password} $db_name <<EOF
-CREATE TABLE patients ( ID int, FirstName varchar(255), LastName varchar(255), Address varchar(255), Contact varchar(255), MedicalIssue  varchar(255), RegDate varchar(255) );
-INSERT INTO patients (ID, FirstName, LastName, Address, Contact, MedicalIssue, RegDate) VALUES ('1', 'abc', 'zxc', 'pune','985241654','Spine','01/01/2021' );
-EOF
+echo "CREATE TABLE mds.patients ( ID int, FirstName varchar(255), LastName varchar(255), Address varchar(255), Contact varchar(255), MedicalIssue  varchar(255), RegDate varchar(255) );" | mysql -h $db_ip -u $db_username -p${db_password} $db_name
+echo "INSERT INTO mds.patients (ID, FirstName, LastName, Address, Contact, MedicalIssue, RegDate) VALUES ('1', 'abc', 'zxc', 'pune', '985241654', 'Spine', '01/01/2021' );" | mysql -h $db_ip -u $db_username -p${db_password} $db_name
 
 sed -i "s|my_host_ip|${db_ip}|" index.php
 sed -i "s|my_user_name|${db_username}|" index.php
